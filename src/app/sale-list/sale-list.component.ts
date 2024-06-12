02aaf7248c71da72fe43b78da1afc8f88e72e830
Alec Willits <alecwillits@gmail.com>
2024-06-12 14:39:49 -0600

    second commit to main
    removed auto generated code for app.comoinent.html, updated sale-list.c.ts to get sales. sale list component.html updated for displaying sales.

--- src/app/sale-list/sale-list.component.ts
+++ src/app/sale-list/sale-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SaleService } from '../sale.service';
 
 @Component({
   selector: 'app-sale-list',
   templateUrl: './sale-list.component.html',
-  styleUrl: './sale-list.component.css'
+  styleUrls: ['./sale-list.component.css']
 })
-export class SaleListComponent {
+export class SaleListComponent implements OnInit {
 
+  sales: any[] = [];
+
+  constructor(private saleService: SaleService) { }
+
+  ngOnInit(): void {
+    this.saleService.getSales().subscribe(
+      data => {
+        this.sales = data;
+      },
+      error => {
+        console.error('Error fetching sales:', error);
+      }
+    );
+  }
 }
