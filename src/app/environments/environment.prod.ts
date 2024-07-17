9a0b3ef4d7cc39b5879c2036c1fc4edbd5e977fc
Alec Willits <alecwillits@gmail.com>
2024-07-16 20:05:14 -0600

    updated env files for local hosting

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,6 @@
+// environment.prod.ts
 export const environment = {
     production: true,
-    apiUrl: 'https://tradevault-backend.onrender.com'
+    apiUrl: 'http://localhost:3000'
   };
   
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 // environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4200/sales'
+  apiUrl: 'http://localhost:3000'
 };
